a588282f70b44441c1b71d4f7381e31bb374c42c
unknown <machineherald007@gmail.com>
2023-05-06 15:50:27 -0400

    removes console log

--- src/util/JSON_to_file.js
+++ src/util/JSON_to_file.js
@@ -4,7 +4,6 @@ const output_dir = path.join(__dirname, "..", "..", "output", "quests.lua")
 const { format } = require("./lua_json")
 
 function JSON_TO_LUA(quests) {
-    console.log(output_dir)
     try {
         const parsed = JSON.parse(JSON.stringify(quests))
         const options = { eol: "\n", singleQuote: false, spaces: 2 }
