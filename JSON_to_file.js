df3d9dab2806bc1bf16fcfb64cb3a122908650f5
unknown <machineherald007@gmail.com>
2023-05-06 08:15:41 -0400

    syntax

--- JSON_to_file.js
+++ JSON_to_file.js
@@ -4,7 +4,7 @@ const { format } = require("lua-json")
 function JSON_TO_LUA(quests) {
     try {
         const parsed = JSON.parse(JSON.stringify(quests))
-        const options = {eol: '\n', singleQuote: false, spaces: 2 }
+        const options = { eol: '\n', singleQuote: false, spaces: 2 }
         const _quests = format(parsed, options)
         return _quests
     } catch(e) {
